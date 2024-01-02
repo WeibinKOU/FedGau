02712d1b7af8c4ce269e1ddb98a6f2c961c34f10
WeibinKOU <wbkou@connect.hku.hk>
2024-01-02 20:13:32 +0800

    small change

---
+++ run_city.sh
@@ -0,0 +1,47 @@
+cp MultiFL/fed_config_BiSeNet_fedavg_cityscapes_no_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_fedavg_cityscapes_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_feddyn0.005_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_feddyn0.01_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_fedgau_cityscapes_no_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_fedgau_cityscapes_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_fedprox0.005_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_BiSeNet_fedprox0.01_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedavg_cityscapes_no_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedavg_cityscapes_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_feddyn0.005_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_feddyn0.01_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedgau_cityscapes_no_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedgau_cityscapes_optim.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedprox0.005_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
+
+cp MultiFL/fed_config_SegNet_fedprox0.01_cityscapes.py MultiFL/fed_config.py
+python fed_train.py
